972d3b1211175f11ece8756b498577876186cdcf
kayquelopes <kaiquelopes.601@gmail.com>
2025-07-12 13:55:37 -0300

    feat: create MenuUtils class

---
+++ src/models/MenuUtils.java
@@ -0,0 +1,62 @@
+package src.models;
+
+public class MenuUtils {
+
+    public static void exibirMenulogin() {
+        System.out.println("=== MUSIC STORE LOGIN ===");
+        System.out.println("1. Realizar Login");
+        System.out.println("2. Criar nova conta");
+        System.out.println("0. Sair");
+    }
+
+    public static void exibirMenuAdministrador() {
+        System.out.println("=== MENU ADMINISTRADOR ===");
+        System.out.println("1. Gerenciar Produtos");
+        System.out.println("2. Gerenciar Vendas");
+        System.out.println("3. Listar Clientes cadastrados");
+
+    }
+
+    public static void exibirMenuProduto() {
+        System.out.println("=== GERENCIAR PRODUTOS ===");
+        System.out.println("1. Cadastrar Produtos");
+        System.out.println("2. Exibir informações do Produto");
+        System.out.println("3. Listar Produtos");
+        System.out.println("4. Alterar Produto");
+        System.out.println("5. Inativar Produto");
+        System.out.println("0. voltar");
+    }
+
+
+    public static void exibirMenuVenda() {
+        System.out.println("=== GERENCIAR VENDAS ===");
+        System.out.println("1. Exibir resumo da venda ");
+        System.out.println("2. Adicionar item à venda");
+        System.out.println("3. Inativar item da venda");
+        System.out.println("4. Finalizar venda");
+        System.out.println("5. Gerar Relatório da venda");
+        System.out.println("0. voltar");
+    }
+
+
+
+    public static void exibirMenuCliente() {
+        System.out.println("=== MENU CLIENTE ===");
+        System.out.println("1 - Cadastrar Cliente");
+        System.out.println("2 - Exibir Dados Cadastrais");
+        System.out.println("3 - Inativar Cliente");
+        System.out.println("4 - Alterar Dados Cadastrais");
+        System.out.println("5 - Consutar produtos");
+        System.out.println("6 - Visualizar Historico de Compras.");
+        System.out.println("0. Sair");
+    }
+
+      public static void exibirMenuConsultaDeProdutos() {
+        System.out.println("=== CONSULTA DE PRODUTOS ===");
+        System.out.println("1. Buscar por nome");
+        System.out.println("2. Buscar por gênero musical");
+        System.out.println("3. Buscar por artista");
+        System.out.println("4. Listar todos os produtos disponíveis");
+        System.out.println("0. Voltar");
+    }
+}
